f5ee6bd6f7b82ee8b1e2fa8966794f3af4c2c07d
ilhembensalha <bensalhailhem@gmail.com>
2021-04-20 03:12:22 +0100

    3

--- app/category.php
+++ app/category.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class produit extends Model
+class category extends Model
 {
     //
 }
---
+++ database/migrations/2021_04_20_020818_create_produits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->double('prix',8,2);
+            $table->enum('size',['s','m','l']);
+            $table->integer('categorie_id')->unsigned();
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('restrict')->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
